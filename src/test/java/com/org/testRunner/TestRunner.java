5b1dc43804697097d46f9ac10de87d250fe4d5f0
lipsa panda <lipsapandap@gmail.com>
2021-07-06 09:18:41 +0530

    modified testrunner file

--- src/test/java/com/org/testRunner/TestRunner.java
+++ src/test/java/com/org/testRunner/TestRunner.java
@@ -1,5 +1,21 @@
 package com.org.testRunner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"./src/test/resources/featuresFiles"},
+		glue= {"com.org.stepDefination", "com.org.appHook"},
+		plugin= {"pretty",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+				
+		})
+
 public class TestRunner {
+	
+	
 
 }
